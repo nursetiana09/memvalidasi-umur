3e0119cbd07969b7a506cbf55805024b3c2c0a64
Nursetiana Situmorang <45727455+nursetiana09@users.noreply.github.com>
2018-12-30 10:23:38 +0700

    Add files via upload

---
+++ memvalidasi usia.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int usia;
+    system("color e");
+        cout<<" validasi usia mnausia "<<endl;
+        cout<<" ################## "<<endl;
+        cout<<" nursetiana situmorang "<<endl;
+        cout<<" masukan usia = ";
+        cin>>usia;
+     if (usia<15)
+        cout<< "anak-anak "<<endl;
+     if (usia=57)
+        cout<<" dewasa " <<endl;
+     else
+        cout<<" lansia ";
+ getch();
+}
